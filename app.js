77d51abd42c641b085e65b80b761e69fb19b73da
Jenny Lin <misslinyang91@gmail.com>
2021-08-25 08:39:28 +1000

    created the questions

---
+++ app.js
@@ -0,0 +1,108 @@
+const start = document.querySelector(".start");
+const quiz = document.querySelector(".quiz");
+const question = document.querySelector(".question");
+const allAnswerChoices = document.querySelectorAll(".choice");
+const answerChoiceA = document.querySelector("#A");
+const answerChoiceA = document.querySelector("#B");
+const answerChoiceA = document.querySelector("#C");
+const answerChoiceA = document.querySelector("#D");
+const counter = document.querySelector(".counter");
+const timeGauge = document.querySelector(".time-gauge");
+const progressContainer = document.querySelector(".progress-container");
+const scoreContainer = document.querySelector(".score-container");
+
+//questions
+let questions = [
+  {
+    question: "How many sounds can a cat make?",
+    questionImg: "img/1.jpg",
+    choiceA: "100",
+    choiceB: "150",
+    choiceC: "10",
+    choiceD: "27",
+    correctAnswer: "100",
+  },
+  {
+    question: "What breed of cat has a reputation for being cross-eyed?",
+    questionImg: "img/2.jpg",
+    choiceA: "Himalayan",
+    choiceB: "Egyptian Mau",
+    choiceC: "Siamese",
+    choiceD: "Persian",
+    correctAnswer: "Siamese",
+  },
+  {
+    question: "What is the most common training command taught to dogs?",
+    questionImg: "img/3.jpg",
+    choiceA: "Stay",
+    choiceB: "Sit",
+    choiceC: "Dance",
+    choiceD: "Roll",
+    correctAnswer: "Sit",
+  },
+  {
+    question: "What is a dog’s most highly developed sense?",
+    questionImg: "img/4.jpg",
+    choiceA: "Smell",
+    choiceB: "Sight",
+    choiceC: "Taste",
+    choiceD: "Touch",
+    correctAnswer: "Smell",
+  },
+  {
+    question: " How many known species of birds are there?",
+    questionImg: "img/5.jpg",
+    choiceA: "5,000",
+    choiceB: "10,000",
+    choiceC: "20,000",
+    choiceD: "40,000",
+    correctAnswer: "10,000",
+  },
+  {
+    question: "What evolutionary adaptation helps birds fly?",
+    questionImg: "img/6.jpg",
+    choiceA: "Rapid Digestion",
+    choiceB: "Beaks",
+    choiceC: "Hollow Bones",
+    choiceD: "All of These",
+    correctAnswer: "All of These",
+  },
+  {
+    question:
+      "Which of the following is not one of the four remaining subspecies of elk in North America?",
+    questionImg: "img/7.jpg",
+    choiceA: "Manitoba Elk",
+    choiceB: "Highland Elk",
+    choiceC: "Rocky Mountain Elk",
+    choiceD: "Tule Elk",
+    correctAnswer: "Highland Elk",
+  },
+  {
+    question: "What is a baby elk called?",
+    questionImg: "img/8.jpg",
+    choiceA: "Bull",
+    choiceB: "Sow",
+    choiceC: "Cow",
+    choiceD: "Calf",
+    correctAnswer: "Calf",
+  },
+  {
+    question: "What do wolves use their scent for?",
+    questionImg: "img/9.jpg",
+    choiceA: "Marking Territory",
+    choiceB: "Finding Prey",
+    choiceC: "A Cover-up",
+    choiceD: "Nothing",
+    correctAnswer: "Marking Territory",
+  },
+  {
+    question:
+      "If a wolf trespasses on the territory of other wolves, what will happen?",
+    questionImg: "img/10.jpg",
+    choiceA: "Nothing",
+    choiceB: "It will be accepted into the pack",
+    choiceC: "It will be chased or killed",
+    choiceD: "It will be required to present prey to the pack",
+    correctAnswer: "It will be chased or killed",
+  },
+];
